77e85f64df3b83e2a756e311b562feee5ae4d820
marcossnikel <marcosnikeldev@gmail.com>
2023-01-19 02:51:09 -0300

    Loops - Python

---
+++ pythonsupport/loops.py
@@ -0,0 +1,20 @@
+n = 0
+#while loops
+while n < 5:
+    print(n)
+    n+=1
+
+#for loops -> looping from i = 0 to i = 4
+
+#incremented in every run of loop
+for i in range(5):
+    print(i)
+
+
+#looping from i=2 to i=5
+for i in range(2,6):
+    print('aa',i)
+
+#reverse -> negative 1 -> decrement
+for i in range(5,1,-1):
+        print(i)
