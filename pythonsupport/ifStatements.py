ae4660255251bc43e701078d6efe95a0d254424c
marcossnikel <marcosnikeldev@gmail.com>
2023-01-19 02:50:56 -0300

    If Statements - Python

---
+++ pythonsupport/ifStatements.py
@@ -0,0 +1,19 @@
+#if statements don´t need parentheses or curly braces
+
+n = 1
+if n > 2:
+    n -= 1
+elif n == 2:
+    n *= 2
+else:
+    n += 2
+
+#parentheses needed for multi-line conditions
+# and == &&
+# or = ||
+
+n,m = 1, 2
+
+if ((n > 2 and
+    n!=m)or n ==m):
+    n+=1
