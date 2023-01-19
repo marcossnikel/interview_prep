ca47e926f258e595e8d481606b6b456986e69402
marcossnikel <marcosnikeldev@gmail.com>
2023-01-19 02:52:13 -0300

    Variables - Python

---
+++ pythonsupport/variables.py
@@ -0,0 +1,20 @@
+#Variables are dinamicly typed
+# dont need to declare nothing
+n = 0
+print(n)
+n = "hi"
+print(n)
+
+#multiple assignmentes
+n,m = 0 , "abc"
+print(n,m)
+#multiple types in same line -> any problem
+
+
+#Increment
+n += 1 #good
+n = n + 1 #good
+
+#null = None
+n = 4
+n = None
